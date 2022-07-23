1026085dcc7f389d18c607c63720e3acad70bc01
Chris Athanas <realityexpander@gmail.com>
2022-07-23 00:56:17 -0500

    Add indicator, fix atan calcs

--- app/src/main/java/com/realityexpander/pickmyweight/MainActivity.kt
+++ app/src/main/java/com/realityexpander/pickmyweight/MainActivity.kt
@@ -6,18 +6,24 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.realityexpander.pickmyweight.ui.theme.PickMyWeightTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var weight by remember { mutableStateOf(80) }
+
             PickMyWeightTheme {
                 Box(
                     modifier = Modifier
@@ -29,10 +35,22 @@ class MainActivity : ComponentActivity() {
                         ),
                         modifier = Modifier
                             .fillMaxWidth()
-                            .align(Alignment.Center)
-                    ) {
-
+                            .height(300.dp)
+                            .align(Alignment.BottomCenter)
+                    ) { weightResult ->
+                        weight = weightResult
                     }
+
+                    Text(
+                        text = "Weight: $weight",
+                        style = TextStyle(
+                            textAlign = TextAlign.Center,
+                            fontSize = 40.sp
+                        ),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.Center)
+                    )
                 }
             }
         }
--- app/src/main/java/com/realityexpander/pickmyweight/WeightElements.kt
+++ app/src/main/java/com/realityexpander/pickmyweight/WeightElements.kt
@@ -4,14 +4,18 @@ import android.graphics.Color
 import android.graphics.Color.argb
 import android.graphics.Paint
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.withRotation
+import kotlin.math.atan2
 import kotlin.math.cos
+import kotlin.math.roundToInt
 import kotlin.math.sin
 
 @Composable
@@ -28,11 +32,41 @@ fun ScaleWidget(
     var center by remember { mutableStateOf(Offset.Zero) } // center of the canvas
     var scaleCircleCenter by remember { mutableStateOf(Offset.Zero) } // center of the scale (off the bottom of the canvas)
 
-    var weightAngle by remember {
-        mutableStateOf(0f)
-    }
+    var weightAngle by remember { mutableStateOf(0f) }
 
-    Canvas(modifier = modifier) {
+    var dragStartAngle by remember { mutableStateOf(0f) }
+    var dragStopAngle by remember { mutableStateOf(0f) }
+
+    Canvas(
+        modifier = modifier
+            .pointerInput(true) {
+                detectDragGestures(
+                    onDragStart = { offset ->
+                        dragStartAngle =
+                            atan2(
+                                scaleCircleCenter.y - offset.y,
+                                scaleCircleCenter.x - offset.x,
+                            ) * (180f / Math.PI.toFloat())
+                    },
+                    onDragEnd = {
+                        dragStopAngle = weightAngle
+                    }
+                ) { change, dragAmount ->
+                    val touchAngle =
+                        atan2(
+                        scaleCircleCenter.y - change.position.y,
+                        scaleCircleCenter.x - change.position.x,
+                    ) * (180f / Math.PI.toFloat())
+
+                    val newAngle = dragStopAngle + (touchAngle - dragStartAngle)
+                    weightAngle = newAngle.coerceIn(
+                        initialWeight - maxWeight.toFloat(),
+                        initialWeight - minWeight.toFloat()
+                    )
+                    onWeightChange((initialWeight - weightAngle).roundToInt())
+                }
+            }
+    ) {
         center = this.center
         scaleCircleCenter = Offset(center.x, scaleWidth.toPx() / 2f + radius.toPx())
         val outerScaleRadius = radius.toPx() + scaleWidth.toPx() / 2f // outer edge of the scale
@@ -60,9 +94,9 @@ fun ScaleWidget(
         }
 
         // Draw weight lines
-        for(i in minWeight..maxWeight) {
+        for (i in minWeight..maxWeight) {
             val angleInDegrees = (i - initialWeight + weightAngle - 90)
-            val angleInRadians = angleInDegrees *  (Math.PI / 180f).toFloat()
+            val angleInRadians = angleInDegrees * (Math.PI / 180f).toFloat()
 
             val lineType = when {
                 i % 10 == 0 -> LineType.TenStep
@@ -74,7 +108,7 @@ fun ScaleWidget(
                 LineType.FiveStep -> style.fiveStepLineLength.toPx()
                 else -> style.normalLineLength.toPx()
             }
-            val lineColor = when(lineType) {
+            val lineColor = when (lineType) {
                 LineType.TenStep -> style.tenStepLineColor
                 LineType.FiveStep -> style.fiveStepLineColor
                 else -> style.normalLineColor
@@ -99,7 +133,8 @@ fun ScaleWidget(
             // Draw weight text
             if (lineType == LineType.TenStep) {
                 drawContext.canvas.nativeCanvas.apply {
-                    val textRadius = (outerScaleRadius - lineLength - 5.dp.toPx() - style.textSize.toPx())
+                    val textRadius =
+                        (outerScaleRadius - lineLength - 5.dp.toPx() - style.textSize.toPx())
                     val x = textRadius * cos(angleInRadians) + scaleCircleCenter.x
                     val y = textRadius * sin(angleInRadians) + scaleCircleCenter.y
 
@@ -113,7 +148,10 @@ fun ScaleWidget(
                             x,
                             y,
                             Paint().apply {
-                                color = argb((lineColor.alpha*255).toInt(), (lineColor.red*255).toInt(), (lineColor.green*255).toInt(), (lineColor.blue*255).toInt())
+                                color = argb((lineColor.alpha * 255).toInt(),
+                                    (lineColor.red * 255).toInt(),
+                                    (lineColor.green * 255).toInt(),
+                                    (lineColor.blue * 255).toInt())
                                 textSize = style.textSize.toPx()
                                 textAlign = Paint.Align.CENTER
                             }
